d75be8736c08b0492818696ed949660d1e3d34f2
Daniel Caujolle-bert <daniel.caujolle-bert@unibas.ch>
2016-09-20 14:31:40 +0200

    Add GPLv2.

--- build.sh
+++ build.sh
@@ -14,6 +14,19 @@ cp -r ./UserLanguages build
 cp gpl-2.0.txt build
 
 cat > build/README.txt <<EOF
+License:
+-------
+  Copyright (c) 2016 Daniel Caujolle-Bert <daniel.caujolle-bert@unibas.ch>, C-CINA <https://c-cina.unibas.ch>, BioEM Lab <https://c-cina.unibas.ch/bioem>
+
+  This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
+
 Installation:
 ------------
 
