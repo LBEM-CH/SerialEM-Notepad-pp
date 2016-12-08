5ae67bee330aab472f3e2bc556db6d41b1df2db0
Daniel Caujolle-Bert <danielvador@gmail.com>
2016-12-08 11:17:57 +0100

    Add missing file.

---
+++ SerialEM4NPP/version.h
@@ -0,0 +1,33 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+namespace AutoVersion{
+	
+	//Date Version Types
+	static const char DATE[] = "08";
+	static const char MONTH[] = "12";
+	static const char YEAR[] = "2016";
+	static const char UBUNTU_VERSION_STYLE[] =  "16.12";
+	
+	//Software Status
+	static const char STATUS[] =  "Release";
+	static const char STATUS_SHORT[] =  "r";
+	
+	//Standard Version Type
+	static const long MAJOR  = 0;
+	static const long MINOR  = 99;
+	static const long BUILD  = 1;
+	static const long REVISION  = 0;
+	
+	//Miscellaneous Version Types
+	static const long BUILDS_COUNT  = 153;
+	#define RC_FILEVERSION 0,99,1,0
+	#define RC_FILEVERSION_STRING "0, 99, 1, 0\0"
+	static const char FULLVERSION_STRING [] = "0.99.1.0";
+	
+	//These values are to keep track of your versioning state, don't modify them.
+	static const long BUILD_HISTORY  = 0;
+	
+
+}
+#endif //VERSION_H
